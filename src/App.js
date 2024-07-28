43061653fb632adaff882f69eb9360250d55e1a9
sanakang0615 <sanakang0615@kaist.ac.kr>
2024-07-28 10:33:47 +0900

    Update Diagram Page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import SeminarFlowDiagram from './components/SeminarFlowDiagram';
 import LandingPage from './components/LandingPage';
+import DiagramPage from './components/DiagramPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <LandingPage/>
+      {/* <LandingPage/> */}
       {/* <SeminarFlowDiagram /> */}
+      <DiagramPage/>
     </div>
   );
 }
